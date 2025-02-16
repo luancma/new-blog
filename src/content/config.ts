a33b2655f3e5035a7719fb8b7a7d6c87d659e6ba
Luan Almeida <luan.almeida@loblaw.ca>
2025-02-16 14:11:46 +0000

    first commit

---
+++ src/content/config.ts
@@ -0,0 +1,19 @@
+import { defineCollection, reference, z } from "astro:content";
+import { glob } from "astro/loaders";
+
+const blog = defineCollection({
+  loader: glob({ base: "./src/data/blog", pattern: "**/*.md" }),
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    author: z.string(),
+    authorDescription: z.string(),
+    tags: z.array(z.string()),
+    primaryTag: z.string(),
+    createdAt: z.string(),
+    authorAvatar: z.string().optional(),
+    references: z.array(z.string()).optional(),
+  }),
+});
+
+export const collections = { blog };
