da8a5797a76775435d2c3727c052897fcf5d8dfe
Luan Almeida <luan.almeida@loblaw.ca>
2025-03-25 15:52:10 -0400

    --wip-- [skip ci]

--- scripts/fetchMDsFiles.js
+++ scripts/fetchMDsFiles.js
@@ -19,12 +19,13 @@ await fetch(`${BACKEND_URL}/md-list`, {
       if (!response.ok) {
         throw new Error(`Failed to download file: ${response.statusText}`);
       }
-      const buffer = await response.buffer();
+      const arrayBuffer = await response.arrayBuffer();
+      const buffer = Buffer.from(arrayBuffer);
+
       fs.writeFileSync(dest, buffer);
     };
 
     const downloadFiles = async (files) => {
-      console.log(files)
       for (const file of files) {
         const fileUrl = file;
         const fileName = fileUrl.split("/").pop();
@@ -32,9 +33,17 @@ await fetch(`${BACKEND_URL}/md-list`, {
         await downloadFile(fileUrl, filePath);
         console.log(`Downloaded ${fileName}`);
       }
-    };    
+    };
+
+    // delete all files in the folder 
+    fs.readdirSync(ASTRO_TARGET_FOLDER).forEach((file) => {
+      fs.unlinkSync(path.join(ASTRO_TARGET_FOLDER, file));
+    });
 
     downloadFiles(res.files).catch((error) => {
       console.error("Error downloading files:", error);
     });
+  })
+  .catch((error) => {
+    console.error("Error fetching files:", error);
   });
--- src/content/config.ts
+++ src/content/config.ts
@@ -2,22 +2,24 @@ import { defineCollection, z } from "astro:content";
 import { glob } from "astro/loaders";
 
 const blog = defineCollection({
-  loader: glob({ base: "./data/blog", pattern: "**/*.md" }),
+  loader: glob({ base: "./data/blog/uploads", pattern: "**/*.md" }),
   schema: z.object({
     title: z.string(),
     description: z.string(),
     author: z.string(),
     authorDescription: z.string(),
     primaryTag: z.string(),
-    createdAt: z.string().optional(),
+    tags: z.array(z.string()),
+    createdAt: z.string(),
+    // Optional fields
     updatedAt: z.string().optional(),
     cover: z.string().optional(),
-    tags: z.array(z.string()).optional(),
     authorAvatar: z.string().optional(),
     references: z.array(z.string()).optional(),
     instagram: z.string().optional(),
     twitter: z.string().optional(),
     linkedin: z.string().optional(),
+    email: z.string().optional(),
   }),
 });
 
